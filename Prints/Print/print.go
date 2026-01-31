ca0bc5b336dd0127a0e734d4ceef9faa5f938fa0
Yasin <ydehfuli@gmail.com>
2026-02-01 00:22:09 +0330

    feat(print): diffs between prints in golang with true directory structure

--- Prints/Print/print.go
+++ Prints/Print/print.go
@@ -3,16 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	customVariable := 10
-
-	// Custom Print
-	fmt.Printf("Custom Print %d\n", customVariable)
-
 	// Print Without EndLine
 	fmt.Print("Hello")
 	fmt.Print("Hello")
-
-	// Print With EndLine & Space between arguments
-	fmt.Println(" Hello World|", "|Hello World With Space!")
-
 }
--- Prints/PrintIn/printIn.go
+++ Prints/PrintIn/printIn.go
@@ -3,15 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	customVariable := 10
-
-	// Custom Print
-	fmt.Printf("Custom Print %d\n", customVariable)
-
-	// Print Without EndLine
-	fmt.Print("Hello")
-	fmt.Print("Hello")
-
 	// Print With EndLine & Space between arguments
 	fmt.Println(" Hello World|", "|Hello World With Space!")
 
