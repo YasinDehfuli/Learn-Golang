07931b1342a88a671bbb0316e2ee44ae11d33fc3
Yasin <ydehfuli@gmail.com>
2026-02-01 13:10:56 +0330

    feat(types): basic types of golang

--- Types/types.go
+++ Types/types.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
 	"math/rand"
 )
 
@@ -95,3 +96,32 @@ func Pointers() {
 }
 
 // ------------- Derived Data Types
+
+//bool
+//
+//string
+//
+//int  int8  int16  int32  int64
+//uint uint8 uint16 uint32 uint64 uintptr
+//
+//byte // alias for uint8
+//
+//rune // alias for int32
+//     // represents a Unicode code point
+//
+//float32 float64
+//
+//complex64 complex128
+// Basic Types of Golang
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+}
