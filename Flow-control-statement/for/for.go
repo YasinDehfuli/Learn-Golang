8a00766cd8eee8be575eb18698485b16417b8a17
Yasin <ydehfuli@gmail.com>
2026-02-01 17:18:41 +0330

    feat(flow-control-statement): optional items of for loop

--- Flow-control-statement/for/for.go
+++ Flow-control-statement/for/for.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+// for init; condition; post // init and post is optional
+func optionalLoop() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
+
 func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
