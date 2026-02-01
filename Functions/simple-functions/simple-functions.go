3f4506da20e0dfc7fc9db525d43ff621c0098467
Yasin <ydehfuli@gmail.com>
2026-02-01 13:05:43 +0330

    feat(functions): variables with initializer outside of functions

--- Functions/simple-functions/simple-functions.go
+++ Functions/simple-functions/simple-functions.go
@@ -2,10 +2,14 @@ package main
 
 import "fmt"
 
+var appName = "Golang App"
+
 func add(x, y, z int) int {
 	return x + y - z
 }
 
 func main() {
 	fmt.Println(add(60, 60, 20))
+
+	fmt.Println(appName)
 }
