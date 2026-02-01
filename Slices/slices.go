2a854320949b538b548e7c99f8c36be4979b08e6
Yasin <ydehfuli@gmail.com>
2026-02-02 01:23:10 +0330

    feat(slices): added usage of slices on array [start:end]

--- Slices/slices.go
+++ Slices/slices.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 
+	x := []bool{true, false, true, true, false, true}
+
+	fmt.Println("bool array =>", x)
 	var s []int = primes[2:5]
-	fmt.Println(s)
+	fmt.Println("primes variable is =>", s)
 }
